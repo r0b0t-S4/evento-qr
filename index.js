854d0610c823586c068bbabcebcc0b258a3c3f77
r0b0t-S4 <samuelguardiaprogramacion@hotmail.com>
2026-06-19 22:10:47 -0600

    fix console log

--- index.js
+++ index.js
@@ -233,5 +233,5 @@ app.get("/dashboard", async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(✅ Servidor corriendo en puerto ${PORT});
+  console.log("Servidor corriendo en puerto" + PORT);
 });
